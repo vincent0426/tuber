ee2a3bed3d54ed00ce001038f6a468f247abb78f
martianwei <martianwei456@gmail.com>
2023-12-07 15:41:17 +0800

    feat: send email

--- business/sys/mail/mail.go
+++ business/sys/mail/mail.go
@@ -1,7 +1,10 @@
 package mail
 
 import (
+	"fmt"
 	"log"
+	"net/smtp"
+	"strings"
 
 	"github.com/TSMC-Uber/server/business/sys/mq"
 	"github.com/streadway/amqp"
@@ -16,5 +19,27 @@ func StartSendEmailWorker() {
 
 func sendEmail(msg amqp.Delivery) {
 	log.Printf("send email to %s", string(msg.Body))
-	// TODO: send email
+
+	from := "tsmcuber@gmail.com"      // sender email address
+	password := "muoq gjoo plxk ejhj" // email password
+	to := string(msg.Body)            // recipient email address
+	subject := "Hello"
+	body := "This is a test email from Go!"
+	// SMTP server configuration.
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	// 電子郵件消息格式
+	message := []byte(
+		"To: " + to + "\r\n" +
+			"Subject: " + subject + "\r\n" +
+			"\r\n" +
+			body + "\r\n",
+	)
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, strings.Split(to, ","), message)
+	if err != nil {
+		fmt.Println("err:", err)
+	}
 }
