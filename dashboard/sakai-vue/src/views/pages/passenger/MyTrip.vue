c05a63c3577c057585130300bfbcb8d0a97389bc
yuweiwang <yuweiwang@ip87-236.vpnip.cc.ntu.edu.tw>
2023-12-11 00:47:04 +0800

    [fix] fix source & destination Name

--- dashboard/sakai-vue/src/views/pages/passenger/MyTrip.vue
+++ dashboard/sakai-vue/src/views/pages/passenger/MyTrip.vue
@@ -79,8 +79,8 @@ const sortField = ref(null);
                         <div class="col-12">
                             <div class="flex flex-column md:flex-row align-items-center p-3 w-full">
                                 <div class="flex-1 text-center md:text-left">
-                                    <div class="font-bold text-2xl">From:{{ slotProps.data.SourceID }}</div>
-                                    <div class="font-bold text-2xl">To:{{ slotProps.data.DestinationID }}</div>
+                                    <div class="font-bold text-2xl">From:{{ slotProps.data.SourceName }}</div>
+                                    <div class="font-bold text-2xl">To:{{ slotProps.data.DestinationName }}</div>
                                     <div class="mb-3">Driver Name:{{ slotProps.data.DriverName }}</div>
                                     <div class="mb-3">Start Time:{{ slotProps.data.StartTime }}</div>
                                     <!-- <Rating :modelValue="slotProps.data.rating" :readonly="true" :cancel="false" class="mb-2"></Rating> -->
