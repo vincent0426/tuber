1824157dc8286c28d9c381d7a9549b25d3120cfd
yuweiwang <yuweiwang@ip87-236.vpnip.cc.ntu.edu.tw>
2023-12-11 00:13:47 +0800

    [Add] Add getMyTrips

--- dashboard/sakai-vue/src/service/TripService.js
+++ dashboard/sakai-vue/src/service/TripService.js
@@ -33,16 +33,26 @@ export class TripService {
             url: '/trips/' + id
         });
     }
-    joinTrip(id) {
+    joinTrip(id,sourceID,destinationID) {
         return request({
             method: 'post',
-            url: '/trips/' + id + '/join'
+            url: '/trips/' + id + '/join',
+            data: {
+                "source_id": sourceID,
+                "destination_id": destinationID
+            }
         });
     }
     getPassenger(id) {
         return request({
             method: 'get',
             url: '/trips/' + id + '/passengers'
+        });
+    }
+    getMyTrips(){
+        return request({
+            method: 'get',
+            url: '/trips/my?status=not_start'
         });
     }
 }
