0de46e9359a55a19a22243986021868504c1b646
neo1202 <neowuhuayo@gmail.com>
2023-12-20 16:44:47 +0800

    [fix] driver history with avatars

--- dashboard/sakai-vue/src/views/pages/driver/DriverHistory.vue
+++ dashboard/sakai-vue/src/views/pages/driver/DriverHistory.vue
@@ -1,45 +1,88 @@
-import Rating from 'primevue/rating';
-<script setup></script>
+<script setup>
+import { useStore } from 'vuex';
+import { onMounted } from 'vue';
+
+const store = useStore();
+const user = store.getters.user;
+console.log(user);
+</script>
 
 <script>
-import { TripService, LocationService } from '@/service';
-const tripService = new TripService();
-const locationService = new LocationService();
-export default {
-    data() {
-        return {
-            rideHistory: []
-        };
-    },
-    mounted() {
-        this.fetchHistory();
-    },
-    methods: {
-        generateRandomRevenue() {
-            const comments = ['31', '52', '10', '12', '14', '15', '20', '24', '31', '55', '84', '85'];
-            return comments[Math.floor(Math.random() * comments.length)];
-        },
-        generateRandomData(response) {
-            response.items.forEach((item) => {
-                item.received_revenue = this.generateRandomRevenue();
-                // item.source_name = this.getLocationName(item.MySourceID);
-                // item.destination_name = this.getLocationName(item.MyDestinationID);
-            });
-        },
-        async fetchHistory() {
-            try {
-                const response = await tripService.getHistory({ trip_status: 'finished', is_driver: false });
-                this.generateRandomData(response);
-                console.log(response);
-                this.rideHistory = response.items;
-            } catch (e) {
-                console.error('Error fetching History:', error);
-            }
-        }
-    }
-};
-</script>
-<!-- <script>
+// import { TripService, DriverService } from '@/service';
+// const tripService = new TripService();
+// const driverService = new DriverService();
+// export default {
+//     data() {
+//         return {
+//             rideHistory: []
+//         };
+//     },
+//     mounted() {
+//         this.fetchHistory();
+//     },
+//     methods: {
+//         generateRandomCost() {
+//             const comments = ['3', '5', '10', '12', '14', '15', '20', '24', '31', '55', '84', '85'];
+//             return comments[Math.floor(Math.random() * comments.length)];
+//         },
+//         async generateNeededData(response) {
+//             await Promise.all(
+//                 response.items.map(async (item) => {
+//                     item.Cost = this.generateRandomCost();
+//                     item.driver_plate = await this.getDriverPlate(item.DriverID);
+//                     // const passengers = await tripService.getPassengers(item.TripID);
+//                     // console.log(passengers.passenger_details);
+//                     // item.passengers = passengers;
+//                 })
+//             );
+//         },
+//         async iterateTripGetData(trips) {
+//             await Promise.all(
+//                 trips.map(async (item) => {
+//                     const passengers = await tripService.getPassengers(item.TripID);
+//                     console.log('passengers:\n', passengers);
+//                     item.trip_user_list = await getTripPassengers(item.tripId);
+//                     // item.trip_driver_name = item.trip_user_list.driver_details.driver_name;
+//                     // const passengerNames = item.trip_user_list.passenger_details
+//                     //     .map((passenger) => passenger.passenger_name)
+//                     //     .filter((passengerName) => passengerName !== item.trip_driver_name)
+//                     //     .join(', ');
+//                     // item.trip_passengers_name = passengerNames;
+//                 })
+//             );
+//             return trips;
+//         },
+//         async getDriverPlate(id) {
+//             const resp = await driverService.getDriver(id);
+//             console.log('getDriverPlateResponse:', resp.plate);
+//             return resp.plate;
+//         },
+//         async fetchHistory() {
+//             try {
+//                 const response = await tripService.getHistory({ trip_status: 'finished', is_driver: true });
+//                 await this.generateNeededData(response);
+//                 const full_response = await iterateTripGetData(response);
+//                 console.log(response);
+//                 console.log('full_response\n', full_response);
+//                 this.rideHistory = response.items;
+//             } catch (e) {
+//                 console.error('Error fetching History:', error);
+//             }
+//         },
+// formatTime(isoDateString) {
+//             const date = new Date(isoDateString);
+//             const options = {
+//                 year: 'numeric',
+//                 month: '2-digit',
+//                 day: '2-digit',
+//                 hour: '2-digit',
+//                 minute: '2-digit'
+//             };
+
+//             return date.toLocaleTimeString([], options);
+//         }
+//     }
+// };
 export default {
     data() {
         return {
@@ -50,22 +93,22 @@ export default {
                     driver_brand: 'Toyota',
                     driver_model: 'Camry',
                     driver_plate: 'ABC123',
-                    source_name: '桃園',
-                    destination_name: '新竹',
+                    SourceName: '桃園',
+                    DestinationName: '新竹',
                     received_revenue: '20',
                     customers: [
                         {
                             customer_img: '../../../assets/images/Sandy.jpg',
                             customer_name: 'Sandy',
-                            customer_source_name: '北車',
+                            customer_source_name: '微風南山',
                             customer_destination_name: '公館',
                             collaboration_time: 2
                         },
                         {
                             customer_img: '../../../assets/images/Patrick.svg',
                             customer_name: 'Patrick',
-                            customer_source_name: '太平洋',
-                            customer_destination_name: '大西洋',
+                            customer_source_name: 'draftland',
+                            customer_destination_name: '公館',
                             collaboration_time: 8
                         }
                     ],
@@ -77,8 +120,8 @@ export default {
                     driver_brand: 'Toyota',
                     driver_model: 'Camry',
                     driver_plate: 'ABC123',
-                    source_name: '台北',
-                    destination_name: '墾丁',
+                    SourceName: '台北凱薩大飯店',
+                    DestinationName: '墾丁',
                     received_revenue: '200',
                     customers: [
                         {
@@ -91,8 +134,8 @@ export default {
                         {
                             customer_img: '../../../assets/images/Patrick.jpg',
                             customer_name: 'Patrick',
-                            customer_source_name: '太平洋',
-                            customer_destination_name: '大西洋',
+                            customer_source_name: '北車',
+                            customer_destination_name: '台中屋馬燒肉',
                             collaboration_time: 8
                         }
                     ],
@@ -100,9 +143,24 @@ export default {
                 }
             ]
         };
+    },
+    methods: {
+        formatTime(isoDateString) {
+            const date = new Date(isoDateString);
+            const options = {
+                year: 'numeric',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit'
+            };
+
+            return date.toLocaleTimeString([], options);
+        }
     }
 };
-</script> -->
+</script>
+
 <template>
     <div>
         <h3 style="text-align: center">Driver History</h3>
@@ -111,10 +169,10 @@ export default {
                 <template #header>
                     <div class="driver-info">
                         <div class="avatar-container">
-                            <img alt="driver avatar" src="../../../assets/images/Patrick.svg" class="avatar" />
+                            <img alt="driver avatar" :src="user.imageURL" class="avatar" />
                         </div>
                         <div class="driver-text">
-                            <p style="font-weight: bold; font-size: 16px">{{ ride.driver_name }}</p>
+                            <p style="font-weight: bold; font-size: 16px">{{ user.name }}</p>
                             <p style="font-size: 13px; font-style: italic">{{ ride.driver_plate }}</p>
                         </div>
                         <img alt="user header" src="../../../assets/images/modelS.jpg" style="width: 130px; height: 100px; object-fit: cover; object-position: center" />
@@ -155,7 +213,7 @@ export default {
                 <template #footer>
                     <div style="height: 50px">
                         <p class="flex justify-content-center">
-                            {{ ride.start_time }}
+                            {{ formatTime(ride.start_time) }}
                         </p>
                         <div class="flex justify-content-center">
                             <p style="font-weight: bold; font-size: 16px" class="m-0">Total revenue: {{ ride.received_revenue }}</p>
